1480e7cacbb721eb7c90f798a4d3a47904668778
pires12455 <viturinokalil@gmail.com>
2021-03-20 14:21:43 -0400

    First commit

---
+++ src/help1.js
@@ -0,0 +1,64 @@
+const help1 = (prefix) => {
+
+	return `
+╭──────────────╮
+ ┣══════𝑺𝒊𝒈𝒂 𝒏𝒂𝒔 𝒓𝒆𝒅𝒆𝒔 𝒔𝒐𝒄𝒊𝒂𝒊𝒔══════┫
+║
+║ _📷 𝐈𝐧𝐬𝐭𝐚𝐠𝐫𝐚𝐦:_ @pireskkkk
+║
+║
+║ 
+║
+║ _🪀𝐆𝐫𝐮𝐩𝐨 𝐝𝐨 𝐰𝐩𝐩:_
+║ _*🌐https://chat.whatsapp.com/CWDE10hFK31JwBN79IWJLB🌐*_
+║
+ ╭╮╮╱▔▔▔▔╲╭╭╮ 
+ ╰╲╲▏▂╲╱▂▕╱╱╯
+ ┈┈╲▏▇▏▕▇▕╱┈┈
+ ┈┈╱╲▔▕▍▔╱╲┈┈ 
+ ╭╱╱ ▕╋╋╋╋▏╲╲╮
+ ╰╯╯┈╲▂▂╱┈╰╰ ╯
+               ☞︎︎︎Bᴏᴛ✌︎︎Mᴇɴᴜ☜︎︎︎
+║
+┗════════════════════════┛ 
+
+╰──────────────╯
+ 
+➸ *${prefix}marcar*
+➸ *${prefix}marcar2*
+➸ *${prefix}marcar3*
+➸ *${prefix}loli*
+➸ *${prefix}loli1*
+➸ *${prefix}hentai*
+➸ *${prefix}dono*
+➸ *${prefix}porno*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde*
+➸ *${prefix}mia*
+➸ *${prefix}mia1*
+➸ *${prefix}mia2*
+➸ *${prefix}belle*
+➸ *${prefix}belle1*
+➸ *${prefix}belle2*
+➸ *${prefix}belle3*
+➸ *${prefix}akeno*
+➸ *${prefix}meme*
+➸ *${prefix}lofi*
+➸ *${prefix}malkova*
+➸ *${prefix}canal*
+➸ *${prefix}nsfwloli*
+➸ *${prefix}reislin*
+➸ *${prefix}limpar*
+➸ *${prefix}marcar*
+➸ *${prefix}ts (texto que deseja transmitir)*
+
+═══════════════════
+
+*Digite ${prefix}dono para mais info*
+════════════════════`
+
+}
+exports.help1 = help1
+
+
